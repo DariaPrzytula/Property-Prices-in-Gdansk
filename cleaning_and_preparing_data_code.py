0bfb8ca50f844c28cc0d0696ad4dfe7d9fd7b247
DariaPrzytula <kurzynka123@gmail.com>
2023-02-08 14:08:38 +0100

    Price of Flat

--- cleaning_and_preparing_data_code.py
+++ cleaning_and_preparing_data_code.py
@@ -1,21 +1,18 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import matplotlib.pyplot as plt
-import plotly.express as px
-import folium
-import plotly.graph_objects as go
-import geopandas as gpd
-from IPython.display import display
-from PIL import Image
 
+np.random.seed(10)
+#%%
 url = ('https://github.com/DariaPrzytula/Property-Prices-in-Gdansk/blob/main/dane_mieszkania_gdansk.xlsx?raw=true')
 df = pd.read_excel(url)
+
+#%%
 
 df['District'] = df['District'].str.split(',').str[0]
 df['Price'] = df['Price'].str.replace('zł', '')
 df['Price per square meter'] = df['Price per square meter'].str.replace('zł/m2', '')
+
+#%%
 
 #uporządkowanie nazw z kolumny "Dzielnica"
 
@@ -32,7 +29,7 @@ df.replace("Gdańsk Św.Wojciech", "Orunia", inplace=True)
 df.replace("Gdańsk Kiełpinek", "Jasień", inplace=True)
 df.replace("Gdańsk Stare Przedmieście", "Śródmieście", inplace=True)
 df.replace("Gdańsk Niedźwiednik", "Brętowo", inplace=True)
-df.replace("Gdańsk Kowale", "Ujeścisko-Łostowice", inplace=TruAe)
+df.replace("Gdańsk Kowale", "Ujeścisko-Łostowice", inplace=True)
 df.replace("Gdańsk Nowe Ogrody", "Śródmieście", inplace=True)
 df.replace("Gdańsk Południe", "Jasień", inplace=True)
 df.replace("Gdańsk Karczemki", "Kokoszki", inplace=True)
@@ -115,19 +112,92 @@ df.replace("Gdańsk Osowa", "Osowa", inplace=True)
 df.replace("Gdańsk  Chełm ", "Chełm", inplace=True)
 df.replace("Gdańsk Przymorze", "Przymorze", inplace=True)
 
+#%%
+
 #uporządkowanie danych z kolumny "Piętro"
 df.replace("2009", np.nan, inplace=True)
 df.replace("2019", np.nan, inplace=True)
 df.replace("2017", np.nan, inplace=True)
 df.replace("2022", np.nan, inplace=True)
+
+#%%
+
 #usunięcie braków
 df= df.dropna()
+
+#%%
+
+# zamiana parter na 0 i typ int
+
+df.replace("parter", "0", inplace=True)
+df["Floor"] = df["Floor"].astype("int64")
+
+
+#%%
 
 # Change types of data
 df['Year'].fillna(0, inplace=True)
 df['Year'] = df['Year'].astype('int64')
 df['Price per square meter'] = df['Price per square meter'].astype('int64')
 
+#%%
+
 # Uporzadkowanie cen. zmiana przecinkow, kropek itp POPRAWIĆ !!! PRZECINKI SĄ GROSZAMI
 
 df['Price'] = df['Price'].replace({' ':'', ',':''}, regex=True).astype('int64')
+df = df.loc[(df['Price'] < 6000000)]
+#%%
+
+df = pd.get_dummies(df, drop_first=True)
+
+#%%
+
+df.to_csv('data_cleaned.csv')
+
+#%%
+
+X = df.copy()
+y = X.pop('Price')
+
+#%%
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+#%%
+
+from sklearn.ensemble import RandomForestRegressor
+
+reg = RandomForestRegressor()
+reg.fit(X_train, y_train)
+
+reg_score = reg.score(X_test,y_test)
+
+#%%
+
+from sklearn.model_selection import GridSearchCV
+
+param_grid = [{'max_depth' : [3, 4, 5, 6, 7, 8, 10, 20],
+               'min_samples_leaf': [3, 4, 5, 10, 15]}]
+
+model = RandomForestRegressor() 
+gs = GridSearchCV(model, param_grid=param_grid, scoring = 'r2')
+gs.fit(X_train, y_train)
+
+#%%
+
+gs_score = gs.score(X_test, y_test)
+
+#%% 
+
+model = gs.best_estimator_
+
+#%%
+
+import pickle
+
+with open('model.pickle', 'wb') as file:
+    pickle.dump(model, file)
+
+ 
