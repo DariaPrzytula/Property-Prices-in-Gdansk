ddc96e3ec38f6e542ddaa9cd4cfe93c7e5f2c918
DariaPrzytula <kurzynka123@gmail.com>
2023-02-08 15:14:00 +0100

    Price of Flat

---
+++ predict_price_of_your_flat.py
@@ -0,0 +1,13 @@
+import dash
+from dash import dcc
+from dash import html
+import plotly.graph_objects as go
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout =
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
